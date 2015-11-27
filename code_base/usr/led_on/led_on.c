94b0f3a61a97ddf0514a9ecf573ef77c90219d72
Your Name <panandr@localhost.localdomain>
2015-11-27 01:58:00 +0100

    fixed led

--- code_base/usr/led_on/led_on.c
+++ code_base/usr/led_on/led_on.c
@@ -6,34 +6,29 @@
 #include <string.h>
 #include "led_on.h"
 #include "leds.h"
-#include "omap_uart.h"
 int main(int argc, char *argv[])
 {
 	errval_t err;
 	
-	debug_printf("led_on: Turning led ON!\n");
+	//printf("led_on: Should have turned led ON!\n");
 	struct capref dev_cap;
 	size_t ret_len;
 
-	while(1) {
-
- 		for (int counter = 0 ;  counter < 250000 ; counter++)
-            __asm("nop");
-
-		printf("h");
-	}	
 	err = aos_rpc_get_dev_cap(get_init_chan(), 0, 0, &dev_cap, &ret_len);
 	if (err_is_fail(err)) {
 		debug_printf("Can not get device frame!\n");
 		abort();
 	}
 
-	debug_printf("Got the device moving on...\n");
-	uint64_t ret_size;	
+	if (capref_is_null(dev_cap))
+		printf("Got null dev?!\n");
+
+	//printf("led_on: Got the device moving on...\n");
+	//uint64_t ret_size;	
 	uint64_t size   = 0x1000;
 	uint64_t offset = 0xA310000;
 	void * vbuf;	
-	err = paging_map_frame(get_current_paging_state(),&vbuf, size, dev_cap, &offset, &ret_size);
+	err = paging_map_frame(get_current_paging_state(),&vbuf, size, dev_cap, &offset, &size);
 	if (err_is_fail(err)) {
 		debug_printf("CAN not map dev frame");
 		abort();
@@ -42,7 +37,6 @@ int main(int argc, char *argv[])
 	led_initialize((lvaddr_t) vbuf);
 
 	turn_on_led1();	
-
-	while(1);	
+	
 	return 0;
 }
--- code_base/usr/led_on/leds.c
+++ code_base/usr/led_on/leds.c
@@ -1,26 +1,26 @@
 
 #include "leds.h"
 
-static volatile uint32_t * gpio1_base;
-static volatile uint32_t * gpio1_oe;
-static volatile uint32_t * gpio1_do;
+static volatile lvaddr_t gpio1_base;
+static volatile lvaddr_t gpio1_oe;
+static volatile lvaddr_t gpio1_do;
 
-void led_initialize(uint32_t mapped_address) {
+void led_initialize(lvaddr_t mapped_address) {
 
-	gpio1_base = (uint32_t *)mapped_address;
+	gpio1_base = mapped_address;
 	gpio1_oe  = gpio1_base + 0x00000134;
 	gpio1_do  = gpio1_base + 0x0000013c;
 
-	debug_printf("difference %d difference %d", gpio1_oe - gpio1_base, gpio1_do- gpio1_base);
-
+	
+	debug_printf("mapped_addreess %p , gpio1_base %p oe = %p do = %p\n", mapped_address, gpio1_base, gpio1_oe, gpio1_do);
 }
 
 void turn_on_led1(void) {
 	
-	debug_printf("content %x\n", *gpio1_do);
-	*gpio1_oe &= ~0x0100;
-	*gpio1_do &= 0x0100;
-	debug_printf("content %x\n", *gpio1_do);
+	//debug_printf("content %x\n", *gpio1_do);
+	* (lvaddr_t *)gpio1_oe &= ~0x0100;
+	* (lvaddr_t *)gpio1_do |= 0x0100;
+	//debug_printf("content %x\n", *gpio1_do);
 	return;
 } 
 
