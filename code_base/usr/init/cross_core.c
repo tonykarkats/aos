1d18e336c2f10191669b0d72f33114d877a3b83b
Your Name <panandr@localhost.localdomain>
2015-11-29 14:50:09 +0100

    Added files for core management, and merged with latest milestone_5 branch

---
+++ code_base/usr/init/cross_core.c
@@ -0,0 +1,8 @@
+#include <cross_core.h>
+
+int get_core_id(struct bootinfo * bi) {
+
+	return bi->coreid;
+
+}
+
---
+++ code_base/usr/init/cross_core.h
@@ -0,0 +1,8 @@
+#include <barrelfish_kpi/types.h>
+#include <barrelfish_kpi/init.h>
+
+int get_core_id(struct bootinfo * bi);
+
+
+
+
